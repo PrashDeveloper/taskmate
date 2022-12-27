e0799232dd69ddbccfb1f577406ae3895e1ea69d
PrashDeveloper <pdayare1510@gmail.com>
2022-12-27 15:18:24 +0530

    Project Completion

---
+++ users_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+from .forms import CustomRegisterForm
+from django.contrib import messages
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        register_form = CustomRegisterForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request,("New User Acccount is created,Login To Get Started!"))
+            return redirect("register")
+    else:       
+        register_form = CustomRegisterForm()
+    return render(request,'register.html', {'register_form': register_form })
