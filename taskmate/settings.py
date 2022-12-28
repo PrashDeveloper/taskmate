c9793a7ca01c44b67d16bb178b9280c456a2cf37
PrashDeveloper <pdayare1510@gmail.com>
2022-12-28 11:54:24 +0530

    Uploading Taskmate Project

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-# from decouple import config
+
 import environ
 env = environ.Env()
 environ.Env.read_env()
